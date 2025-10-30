10025e0322af928f5129d441d4b75680895a5506
rahmatawan <r.kurniawan416@students.amikom.ac.id>
2025-10-30 22:41:37 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,26 +9,30 @@ int main()
     string teks;
     bool palindrom = true;
 
-    cout << "enter teks: ";
-    getline(cin, teks);
-    int n = teks.length();
+    whlie (true)
+    {
+        cout << "enter teks: ";
+        getline(cin, teks);
+        int n = teks.length();
 
-    for (int i = 0; i < n / 2; i++) {
-        for (int j = n - 1 - i; j < n; j++) {
-            if (isalnum(teks[i] && teks[j])) {
-                if (tolower(teks[i]) != tolower(teks[j])) {
-                    palindrom = false;
-                    break;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = n - 1 - i; j < n; j++) {
+                if (isalnum(teks[i] && teks[j])) {
+                    if (tolower(teks[i]) != tolower(teks[j])) {
+                        palindrom = false;
+                        break;
+                    }
                 }
             }
         }
-    }
 
-    if (!palindrom)
-        cout << "\"" << teks << "\"" << " is not palindrome." << endl;
-    else
-        cout << "\"" << teks << "\"" << " is palindrome." << endl;
-   
-    system("pause");
-    return 0;
+        if (!palindrom)
+            cout << "\"" << teks << "\"" << " is not palindrome." << endl;
+        else
+             cout << "\"" << teks << "\"" << " is palindrome." << endl;
+    }
+    
+        system("pause");
+        return 0;
 }
+
