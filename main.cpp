01a9b69161bd31b44dd0cd14186107cbff980103
rahmatawan <r.kurniawan416@students.amikom.ac.id>
2025-10-30 22:34:16 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main() 
+{
+    string teks;
+    bool palindrom = true;
+
+    cout << "enter teks: ";
+    getline(cin, teks);
+    int n = teks.length();
+
+    for (int i = 0; i < n / 2; i++) {
+        for (int j = n - 1 - i; j < n; j++) {
+            if (isalnum(teks[i] && teks[j])) {
+                if (tolower(teks[i]) != tolower(teks[j])) {
+                    palindrom = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (!palindrom)
+        cout << "\"" << teks << "\"" << " is not palindrome." << endl;
+    else
+        cout << "\"" << teks << "\"" << " is palindrome." << endl;
+   
+    system("pause");
+    return 0;
+}
